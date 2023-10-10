fcd92d52491f94e66db87045abc8a9a23d77faa5
Ti_ymep <alekseu008@gmail.com>
2023-10-10 21:38:04 +0300

    Gotovo Pr3

--- my_work/WindowsFormsApp1/Form1.cs
+++ my_work/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,44 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            // Показываем диалог открытия. Если нажать "ОК" то выведет изображение
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+            // Показываем диалог открытия выбора цвета. Если нажать "ОК" то сменится фон
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Отчистить форму
+            pictureBox1.Image = null;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            // Закрыть форму
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // Расширяем изображение во все окно
+            if (!checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode |= PictureBoxSizeMode.Normal;
+        }
     }
 }
