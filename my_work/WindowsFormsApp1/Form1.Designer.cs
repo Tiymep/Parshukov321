71d5ae62bbec0e3b769c53ac6de72accfba0dacb
Алексей Паршуков <alekseu008@gmail.com>
2023-11-11 09:58:36 +0500

    Практика 9

--- my_work/WindowsFormsApp1/Form1.Designer.cs
+++ my_work/WindowsFormsApp1/Form1.Designer.cs
@@ -29,12 +29,108 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.txtString = new System.Windows.Forms.TextBox();
+            this.txtKolvo = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.scet1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtString
+            // 
+            this.txtString.Location = new System.Drawing.Point(137, 12);
+            this.txtString.Name = "txtString";
+            this.txtString.Size = new System.Drawing.Size(132, 20);
+            this.txtString.TabIndex = 0;
+            // 
+            // txtKolvo
+            // 
+            this.txtKolvo.Location = new System.Drawing.Point(137, 81);
+            this.txtKolvo.Name = "txtKolvo";
+            this.txtKolvo.Size = new System.Drawing.Size(77, 20);
+            this.txtKolvo.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // scet1
+            // 
+            this.scet1.Location = new System.Drawing.Point(158, 121);
+            this.scet1.Name = "scet1";
+            this.scet1.Size = new System.Drawing.Size(111, 24);
+            this.scet1.TabIndex = 3;
+            this.scet1.Text = "Подсчитать";
+            this.scet1.UseVisualStyleBackColor = true;
+            this.scet1.Click += new System.EventHandler(this.scet1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введи предложение";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Количество Символов";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Введите символ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(137, 44);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(281, 159);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.scet1);
+            this.Controls.Add(this.txtKolvo);
+            this.Controls.Add(this.txtString);
+            this.Name = "Form1";
+            this.Text = "x";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtString;
+        private System.Windows.Forms.TextBox txtKolvo;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button scet1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- my_work/WindowsFormsApp1/Form1.cs
+++ my_work/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,39 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void scet1_Click(object sender, EventArgs e)
+        {
+            txtKolvo.Text = NumberSymbols(txtString.Text, 'а').ToString();
+            txtKolvo.Text = NumberMA(txtString.Text).ToString();
+        }
+
+        private int NumberSymbols(string stroka, char symbol)
+        {
+            int k = 0;
+            for (int i = 0; i < stroka.Length; i++)
+            {
+                if (stroka[i] == symbol)
+                {
+                    k += 1;
+                }
+            }
+            return k;
+        }
+
+        private int NumberMA(string stroka)
+        {
+            int ma = 0;
+            for (int i = 0; i < txtString.Text.Length - 1; i++)
+            {
+                if (txtString.Text[i] == 'м' && txtString.Text[i + 1] == 'а')
+                {
+                    ma++;
+                }
+            }
+            return ma;
+        }
     }
 }
+
+    
